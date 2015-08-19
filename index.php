b3682d89c7e76015505b60518bcac63341d4926d
トゥムルック <Tumulucc@gmail.com>
2015-08-19 15:13:16 +0900

    comment2

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-﻿<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>PHP Test</title>
+</head>
+  <style>
+    body{
+      width:100%;
+      text-align:center;
+      font-size:200%;
+      line-height:100vh;
+    }
+  </style>
+<body>
+<?php
 echo "Hello Heroku World!";
 ?>
+</body>
+</html>
